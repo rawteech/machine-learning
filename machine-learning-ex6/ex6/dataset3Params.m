41598f756bc18c93ce280bd6cf548016888086fe
rawteech <rotichbil636@gmail.com>
2017-02-03 08:22:33 +0300

    Gausian Kernel Decision boundary

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,10 +23,41 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+% possible ranges of C and sigma
+C_range = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+Sigma_range = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+% optimal ranges of C and sigma
+optimal_C = C_range(1);
+optimal_sigma = Sigma_range(1);
 
+% previous error
+previous_error = 1000;
 
+% now to get optimal C and Sigma we loop through
+for i=1:length(C_range)
+    
+    now_c = C_range(i);
+    
+    for j=1:length(Sigma_range)
+        
+        currentSigma = Sigma_range(j);
+        model = svmTrain(X, y, now_c, @(x1, x2) gaussianKernel(x1, x2, currentSigma));
+        predictions = svmPredict(model, Xval);
+        err = mean(double(predictions ~= yval));
+        
+        if err < previous_error
 
+            optimal_C = now_c;
+            optimal_sigma = currentSigma;
+            previous_error = err;
+        end;
+    end;
+end;
+
+% our final returned values required
+C = optimal_C;
+sigma = optimal_sigma;
 
 
 % =========================================================================
